c804c89aacebe5606e2822504b513ef26fb2b566
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-10-01 18:13:45 +0000

    Add Comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
+/**
+ * Detect when user scrolls past 400px and shrink the nav and logo
+ */
 window.addEventListener('scroll', function () {
     let scrollPos = window.scrollY;
     if (scrollPos > 400) {
-       document.getElementById("header").classList.add("shrink");
-       document.getElementById("logo").classList.add("shrink");
+        document.getElementById("header").classList.add("shrink");
+        document.getElementById("logo").classList.add("shrink");
     } else {
         document.getElementById("header").classList.remove("shrink");
         document.getElementById("logo").classList.remove("shrink");
     }
-  });
+});
